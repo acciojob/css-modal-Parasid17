5d8213c8f379b9daa22ee42d804ab548adbab9b5
Siddhant Herale <154230247+Parasid17@users.noreply.github.com>
2025-10-31 19:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const modal = document.getElementById("myModal");
+    const openBtn = document.getElementById("openModal");
+    const closeBtn = document.querySelector(".close-modal");
+
+    openBtn.onclick = () => {
+      modal.style.display = "flex";
+    };
+
+    closeBtn.onclick = () => {
+      modal.style.display = "none";
+    };
+
+    window.onclick = (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    };
